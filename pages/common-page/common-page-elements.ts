375a3b5f8f0dc0f0bda2d6926111eee4e398cca7
corintogalvan93 <corintogalvan93@gmail.com>
2026-04-22 13:32:02 -0600

    Class Common Page Elements

---
+++ pages/common-page/common-page-elements.ts
@@ -0,0 +1,29 @@
+import { Page } from "@playwright/test"
+
+export class CommonPageElements{
+    private page: Page
+
+    constructor(page: Page){
+        this.page= page
+
+    }
+
+    get leftMenu(){
+        return{
+            allItems: this.page.locator('#inventory_sidebar_link'),
+            about: this.page.locator('#about_sidebar_link'),
+            logOut: this.page.locator('#logout_sidebar_link'),
+            resetAppState: this.page.locator('#reset_sidebar_link')
+        },
+    }
+
+    get Icons(){
+        return {
+            openMenu: this.page.locator('.bm-burger-button')
+
+        }
+
+
+
+    }
+}
